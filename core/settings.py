ec145240602ef97747d163599c41602f6c21c37d
JackMaloiDesenvolvedor <jackmaloidesenvolvedor@gmail.com>
2024-02-12 11:15:56 -0300

    Try new package for statics files

--- core/settings.py
+++ core/settings.py
@@ -23,19 +23,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wpd*b&8l-0odx&e8+q^bh5tka-2-xp!ee4u+pb#v3a5&7*y!ku'
+#SECRET_KEY = 'django-insecure-wpd*b&8l-0odx&e8+q^bh5tka-2-xp!ee4u+pb#v3a5&7*y!ku'
 
 # # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # database_url = os.environ.get("DATABASE_URL")
 
